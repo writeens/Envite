d93ccc49f423a9ab6575a6d879cc75335b559aba
Adewusi Victor <glove.adewusi@gmail.com>
2022-03-30 22:10:50 +0100

    Setup Create Account Screen

---
+++ app/src/main/java/com/example/envite/activities/CreateAccountActivity.java
@@ -0,0 +1,46 @@
+package com.example.envite.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.envite.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.createAccountToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    public void onClickLogIn (View view) {
+        //GO TO LOGIN ACTIVITY
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
--- app/src/main/java/com/example/envite/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/envite/activities/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.envite.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.envite.R;
 
@@ -12,5 +14,10 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    public void onClickSignUp (View view) {
+        Intent intent = new Intent(getApplicationContext(), CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
