caa122a8c8f04e823fe02f70be38cd519d788424
Adewusi Victor <glove.adewusi@gmail.com>
2022-04-09 17:09:40 +0100

    Setup sent envite screen

---
+++ app/src/main/java/adapters/SentEnviteListAdapter.java
@@ -0,0 +1,88 @@
+package adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.envite.R;
+
+import entities.Envite;
+
+public class SentEnviteListAdapter extends RecyclerView.Adapter<SentEnviteListAdapter.ViewHolder> {
+
+    private Envite[] dataSet;
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView cardThumbnailImageView;
+        private final TextView cardHeaderTextView;
+        private final TextView cardSubheadTextView;
+        private final ImageView cardMediaImageView;
+        private final TextView cardSupportingTextView;
+
+
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            cardThumbnailImageView = (ImageView) view.findViewById(R.id.enviteCardThumbnail);
+            cardHeaderTextView = (TextView) view.findViewById(R.id.enviteCardHeaderText);
+            cardSubheadTextView = (TextView) view.findViewById(R.id.enviteCardSubhead);
+            cardMediaImageView = (ImageView) view.findViewById(R.id.enviteCardMedia);
+            cardSupportingTextView = (TextView) view.findViewById(R.id.enviteCardSupportingText);
+        }
+
+        public ImageView getCardThumbnailImageView() {
+            return cardThumbnailImageView;
+        }
+        public TextView getCardHeaderTextView() {
+            return cardHeaderTextView;
+        }
+        public TextView getCardSubheadTextView() {
+            return cardSubheadTextView;
+        }
+        public ImageView getCardMediaImageView() {
+            return cardMediaImageView;
+        }
+        public TextView getCardSupportingTextView() {
+            return cardSupportingTextView;
+        }
+    }
+
+    public SentEnviteListAdapter(Envite[] dataSet, Context ctx) {
+        this.dataSet = dataSet;
+        this.context = ctx;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.envite_card_item_v2, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.getCardHeaderTextView().setText(dataSet[position].getTitle());
+        holder.getCardSupportingTextView().setText(dataSet[position].getNote());
+        holder.getCardSubheadTextView().setText(dataSet[position].getFormattedPrice());
+        Glide.with(context).load("https://res.cloudinary.com/brees/image/upload/v1648084145/static/header-final_gxcnux.png").into(holder.getCardThumbnailImageView());
+        Glide.with(context).load("https://res.cloudinary.com/brees/image/upload/v1648084145/static/header-final_gxcnux.png").into(holder.getCardMediaImageView());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataSet.length;
+    }
+}
--- app/src/main/java/fragments/SentEnviteFragment.java
+++ app/src/main/java/fragments/SentEnviteFragment.java
@@ -3,6 +3,8 @@ package fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,57 +12,81 @@ import android.view.ViewGroup;
 
 import com.example.envite.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SentEnviteFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import adapters.EnviteListAdapter;
+import adapters.SentEnviteListAdapter;
+import entities.Envite;
+
 public class SentEnviteFragment extends Fragment {
+    private static final String KEY_LAYOUT_MANAGER = "layoutManager";
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SentEnviteFragment() {
-        // Required empty public constructor
+    private enum LayoutManagerType {
+        LINEAR_LAYOUT_MANAGER
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SentEnviteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SentEnviteFragment newInstance(String param1, String param2) {
-        SentEnviteFragment fragment = new SentEnviteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    protected RecyclerView mRecyclerView;
+    protected SentEnviteListAdapter mAdapter;
+    protected RecyclerView.LayoutManager mLayoutManager;
+    protected SentEnviteFragment.LayoutManagerType mCurrentLayoutManagerType;
+    protected Envite[] mDataset;
+    private static final int DATASET_COUNT = 10;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+        //INITIALIZE DATASET
+        initDataset();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sent_envite, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_sent_envite, container, false);
+
+        // BEGIN_INCLUDE(initializeRecyclerView)
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.sentEnviteRecyclerView);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+
+        mCurrentLayoutManagerType = SentEnviteFragment.LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        if (savedInstanceState != null) {
+            // Restore saved layout manager type.
+            mCurrentLayoutManagerType = (SentEnviteFragment.LayoutManagerType) savedInstanceState
+                    .getSerializable(KEY_LAYOUT_MANAGER);
+        }
+
+        setRecyclerViewLayoutManager(mCurrentLayoutManagerType);
+
+        mAdapter = new SentEnviteListAdapter(mDataset, getContext());
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        return rootView;
+    }
+
+    public void setRecyclerViewLayoutManager(SentEnviteFragment.LayoutManagerType layoutManagerType) {
+        int scrollPosition = 0;
+
+        // If a layout manager has already been set, get current scroll position.
+        if (mRecyclerView.getLayoutManager() != null) {
+            scrollPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager())
+                    .findFirstCompletelyVisibleItemPosition();
+        }
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mCurrentLayoutManagerType = SentEnviteFragment.LayoutManagerType.LINEAR_LAYOUT_MANAGER;
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.scrollToPosition(scrollPosition);
+    }
+
+    private void initDataset() {
+        mDataset = new Envite[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = new Envite((i+1) + " bedroom Flat with amenities and everything you need", "Another beautiful apartment available for your perusal at " + (i+1), (i * 100));
+        }
     }
 }
